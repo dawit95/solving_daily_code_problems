8ca5837008dcdb5c86c6b297c690b1a1f4076d71
dawit95 <dawit0310@gmail.com>
2021-12-02 23:35:13 +0900

    [ feat ] Add sol17298

--- baekjoon/src/com/baekjoon/sol17298/Main.java
+++ baekjoon/src/com/baekjoon/sol17298/Main.java
@@ -1,10 +1,36 @@
 package com.baekjoon.sol17298;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        int[] arr = new int[n];
+        String[] input = br.readLine().trim().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+        br.close();
+
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while(!stack.isEmpty() && arr[stack.peek()] < arr[i]){
+                ans[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        // answer
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(ans[i]==0?-1:ans[i]).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
