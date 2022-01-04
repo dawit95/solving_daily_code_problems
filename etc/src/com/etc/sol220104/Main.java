8f4c5ed3a15ab750a092c1b7c20e49bf71ad4dcb
dawit95 <dawit0310@gmail.com>
2022-01-05 00:29:51 +0900

    [ feat ] modify sol220104

--- etc/src/com/etc/sol220104/Main.java
+++ etc/src/com/etc/sol220104/Main.java
@@ -11,62 +11,61 @@ public class Main {
         ans = 0;
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 4; j++) {
-                schedule[i][j] = schedule[i][j].replace(":","");
+                schedule[i][j] = schedule[i][j].replace(":", "");
             }
         }
-        okPermu(new int[5], 0);
+        HashSet<String> set = new HashSet<>();
+        okPermu(set, new int[5], 0);
 
         System.out.println(ans);
     }
 
-    private static void okPermu(int[] arr, int idx) {
+    private static void okPermu(HashSet<String> set, int[] arr, int idx) {
         if (idx == 5) {
-            if (check(arr)) {
-                ans++;
-            }
+            ans++;
             return;
         }
         for (int i = 0; i < 4; i++) {
-            arr[idx] = i;
-            okPermu(arr, idx + 1);
-        }
-    }
-
-    private static boolean check(int[] arr) {
-        HashSet<String> set = new HashSet<>();
-        for (int i = 0; i < 5; i++) {
-            String str = schedule[i][arr[i]];
-            if(str.length()>10){
-                String str1 = str.substring(0,7);
-                String str2 = str.substring(8,15);
+            String str = schedule[idx][i];
+            HashSet<String> tmpSet = new HashSet<>(set);
+            boolean flag = true;
+            if (str.length() > 10) {
+                String str1 = str.substring(0, 7);
+                String str2 = str.substring(8, 15);
                 for (int j = 0; j < 3; j++) {
-                    if (set.contains(str1))
-                        return false;
-                    set.add(str1);
+                    if (tmpSet.contains(str1)) {
+                        flag = false;
+                        break;
+                    }
+                    tmpSet.add(str1);
                     str1 = setting(str1);
-                    if (set.contains(str2))
-                        return false;
-                    set.add(str2);
+                    if (tmpSet.contains(str2)) {
+                        flag = false;
+                        break;
+                    }
+                    tmpSet.add(str2);
                     str2 = setting(str2);
                 }
-            }else{
+            } else {
                 for (int j = 0; j < 6; j++) {
-                    if (set.contains(str))
-                        return false;
-                    set.add(str);
+                    if (tmpSet.contains(str)) {
+                        flag = false;
+                        break;
+                    }
+                    tmpSet.add(str);
                     str = setting(str);
                 }
             }
+            if (flag)
+                okPermu(tmpSet, arr, idx + 1);
         }
-        return true;
     }
-
     private static String setting(String str) {
-        StringBuilder sb = new StringBuilder(str.substring(0,3));
-        String tmp = str.substring(3,7);
+        StringBuilder sb = new StringBuilder(str.substring(0, 3));
+        String tmp = str.substring(3, 7);
         int time = Integer.parseInt(tmp);
-        time += time%100==0?30:100;
-        if(time<1000)
+        time += time % 100 == 0 ? 30 : 100;
+        if (time < 1000)
             sb.append(0);
         sb.append(time);
         return sb.toString();
